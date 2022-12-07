bc9945a57b530246581f899aa35214ad28728df5
Oscar Veldman <oveldman@gmail.com>
2022-12-07 21:31:25 +0100

    Add deployment script

---
+++ MadWorld/MadWorld.Deployment/deploy-madworld.sh
@@ -0,0 +1,11 @@
+subscriptionName="Oscar"
+resourceGroupName="MadWorldTools"
+
+az account set --subscription subscriptionName
+az group create --name $resourceGroupName --location westeurope
+
+az deployment group create \
+  --name deployment-madworld \
+  --resource-group $resourceGroupName \
+  --template-uri "https://raw.githubusercontent.com/oveldman/MadWorldTools/master/MadWorld/MadWorld.Deployment/deploy-madworld.json" \
+  --parameters storageAccountType=Standard_LRS
