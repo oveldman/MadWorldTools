8c5e7f8d75b4c0a0ebb3472b88c61f65f3a01b63
Oscar Veldman <oveldman@gmail.com>
2022-12-11 16:15:38 +0100

    Add mode to deployment

--- MadWorld/MadWorld.Deployment/deploy-madworld.sh
+++ MadWorld/MadWorld.Deployment/deploy-madworld.sh
@@ -5,6 +5,7 @@ az account set --subscription $subscriptionName
 az group create --name $resourceGroupName --location westeurope
 
 az deployment group create \
+  --mode Complete \
   --name deployment-madworld \
   --resource-group $resourceGroupName \
   --template-uri "https://raw.githubusercontent.com/oveldman/MadWorldTools/master/MadWorld/MadWorld.Deployment/deploy-madworld.json"
